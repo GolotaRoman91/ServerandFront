c6f607c4215b6c5e86ff1798a80b14a1fa2ad2af
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2021-12-27 15:51:48 +0200

    add some styles

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,17 @@ const host = 'localhost';
 const port = 8000;
 
 const server = createServer(async (request, response) => {
-  // response.setHeader('Access-Control-Allow-Origin', '*');
-  // response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  // response.setHeader('Content-Type', 'application/json, text/plain; charset=utf-8;');
-  // response.setHeader('Access-Control-Max-Age', '-1');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  response.setHeader('Content-Type', 'application/json, text/plain; charset=utf-8;');
+  response.setHeader('Access-Control-Max-Age', '-1');
 
   if (request.method === 'OPTIONS') {
     response.statusCode = 200;
     return response.end();
   }
-
-  // response.writeHead(200);
-
   router(request, response);
-  // console.log(process.env);
 });
 
 server.listen(port, host, () => {
